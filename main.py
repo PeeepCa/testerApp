ee70ae585decfd4f4b95b2d197451ce1f8795bf1
PeeepCa <josef.pokorny@hotmail.com>
2024-09-19 12:49:43 +0200

    Start

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # TesterAPP
 
 # TODO
-#  Multithreading
-#  GUI
-#  Logging
-#  Error handling
-#  iTAC
-#  SESO
+#  Multithreading for parallel testing
+#  GUI with dynamic UI for testing
+#  Config file stored on server
+#  Auto updating like PEPO
+#  Logging to server log file
+#  Error handling inside the APP
+#  iTAC via restAPI
+#  SESO via restAPI
 #  HW library
+#  Muster
