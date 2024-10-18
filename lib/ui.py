db508184076b547d1e6b0355625e665c1185f4e1
PeeepCa <josef.pokorny@hotmail.com>
2024-10-18 11:58:01 +0200

    v0.071 - Started with UI - Added status bar dummy and file menu dummy

--- lib/ui.py
+++ lib/ui.py
@@ -1,14 +1,37 @@
 import tkinter as tk
+from tkinter import ttk
 
 import lib.shared_variables
 
 
 class UI:
     def __init__(self):
+        # Create main window
         self.root = tk.Tk()
-        self.root.title("Basic Tkinter Window")
+        self.root.title("APAG Tester App")
+        self.root.geometry(f'640x480')
+        # self.root.config(bg='white')
+        # self.root.anchor('center')
+
+        # Create listbox
         self.listbox = tk.Listbox(self.root)
         self.listbox.pack()
+
+        # Create progressbar
+        self.progressbar = ttk.Progressbar(self.root, orient="horizontal", length=500, mode="indeterminate")
+        self.progressbar.place(x=70, y=410)
+        # self.progressbar.pack()
+
+        # Create menubar
+        self.menubar = tk.Menu(self.root)
+        self.root.config(menu=self.menubar)
+
+        # Create file menu
+        self.file_menu = tk.Menu(self.menubar, tearoff=0)
+        self.file_menu.add_command(label="Exit", command=self.root.quit)
+        self.menubar.add_cascade(label="File", menu=self.file_menu)
+
+        # Others
         self.update_interval = 100  # 1000 milliseconds = 1 second
         self.step_prev = ''
         self.update_text()
